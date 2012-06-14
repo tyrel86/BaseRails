180642a84db9387ef648c1c7494352037f064464
Tyrel Richey <tyrelrichey@gmail.com>
2012-06-14 15:11:18 -0600

    The begining

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.5'
+gem 'mysql2'
+gem 'jquery-rails'
+gem 'bcrypt-ruby', '~> 3.0.0'
+
+group :development do
+	gem 'debugger'
+end
+
+group :bdd do
+	gem 'rspec-rails'
+	gem 'cucumber-rails', :require => false
+	gem 'database_cleaner'
+	gem 'factory_girl'
+	gem 'nokogiri'
+	gem 'capybara'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CannaPages::Application.config.secret_token = 'c459948185b80aeb43220f0503923821f5f1a4f42229fa8236c886bff1267aec554cdfa4ff7ba3d51f89dfbf6e4012d7e43af7231a502ea679de448d36112295'
